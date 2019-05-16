5b19622e00c86f25f0287b66293c82153274bc2e
Paulo Roberto Candido dos Santos <psantos@psantos.eti.br>
2019-05-16 01:58:00 -0300

    cleanup

--- icinga2_supervisord.sh
+++ icinga2_supervisord.sh
@@ -2,5 +2,4 @@
 service icinga2 foreground & sleep 15 &&
 # Allow any signal which would kill a process to stop server
 trap "service icinga2 stop" HUP INT QUIT ABRT ALRM TERM TSTP
-#while pgrep -u nagios icinga2 > /dev/null; do sleep 5; done
-while pgrep -u nagios icinga2 > /dev/null; do sleep 10; done
+while pgrep -u nagios icinga2 > /dev/null; do sleep 5; done
