0194f10cc9ec50c77a2cff1e58c975f930a23f22
Paulo Roberto Candido dos Santos <psantos@psantos.eti.br>
2019-05-16 00:21:34 -0300

    Been using jjethwa/icinga2 init scripts but needed to add a little more time for the daemon to start to use the while pgrep method jjethwa is using. Probably because of different versions.

--- icinga2_supervisord.sh
+++ icinga2_supervisord.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-service icinga2 foreground &
+service icinga2 foreground & sleep 5 &&
 # Allow any signal which would kill a process to stop server
 trap "service icinga2 stop" HUP INT QUIT ABRT ALRM TERM TSTP
+#while pgrep -u nagios icinga2 > /dev/null; do sleep 5; done
 while pgrep -u nagios icinga2 > /dev/null; do sleep 5; done
